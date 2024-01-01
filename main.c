483388d7f260171156afac48fca4e2f81ff196b5
saurabh <kapanarinsal6@gmail.com>
2024-01-01 13:10:32 +0530

    added commentcode.c (in the previous commit i write wrong commit message

--- main.c
+++ main.c
@@ -3,7 +3,7 @@ int main(){
     int i=0,n;
     printf("enter value: ");scanf("%d",&n);
     while(i<n+1){
-        printf("%d\n",i);i++;fflush(stdout); //flush the standard output buffer(by chatgpt)
+        printf("%d\n",i);i++;fflush(stdout); //flush the standard output buffer(by chatgpt) because the printf had no character
     }
     return 0;
 }
